ec21719e80b32a3ccb73c85332b6ca3380cea7f6
alirezayoom <alirezayoom@gmail.com>
2023-06-16 15:19:37 +0430

    add questionNumber

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,8 @@ List<String> questions = [
   'A slug\'s blood is green.',
 ];
 
+int questionNumber = 0;
+
 class _QuizPageState extends State<QuizPage> {
   @override
   Widget build(BuildContext context) {
@@ -44,7 +46,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[0],
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -68,10 +70,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
-                  scoreKeeper.add(Icon(
-                    Icons.check,
-                    color: Colors.green,
-                  ));
+                  questionNumber++;
                 });
               },
             ),
@@ -90,7 +89,9 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                setState(() {
+                  questionNumber++;
+                });
               },
             ),
           ),
