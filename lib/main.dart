25e986ff1370ab454342dfb4075a288f440df8c4
alirezayoom <alirezayoom@gmail.com>
2023-06-16 15:11:22 +0430

    add questions list

--- lib/main.dart
+++ lib/main.dart
@@ -24,15 +24,11 @@ class QuizPage extends StatefulWidget {
   _QuizPageState createState() => _QuizPageState();
 }
 
-List<Icon> scoreKeeper = [
-  Icon(
-    Icons.check,
-    color: Colors.green,
-  ),
-  Icon(
-    Icons.close,
-    color: Colors.red,
-  ),
+List<Icon> scoreKeeper = [];
+List<String> questions = [
+  'You can lead a cow down stairs but not up stairs.',
+  'Approximately one quarter of human bones are in the feet.',
+  'A slug\'s blood is green.',
 ];
 
 class _QuizPageState extends State<QuizPage> {
@@ -48,7 +44,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[0],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -107,9 +103,3 @@ class _QuizPageState extends State<QuizPage> {
     );
   }
 }
-
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
